1ea79046153f800bd5e10c4205091029e47b317a
Merc5Dark <31131794siya@gmail.com>
2023-10-23 08:54:42 +0200

    api key not working

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,45 @@
-import { Configuration, OpenAIApi } from "openai";
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
 
 const app = express();
 const port = 8000;
+
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-const configuration = new Configuration({
-  organization: "org-0nmrFWw6wSm6xIJXSbx4FpTw",
-  apiKey: "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",
+// OpenAI Configuration
+const openaiConfig = new Configuration({
+  apiKey: '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',
 });
-const openai = new OpenAIApi(configuration);
 
-app.post("/", async (request, response) => {
+const openai = new OpenAIApi(openaiConfig);
+
+// Define the API endpoint
+app.post('/', async (request, response) => {
   const { chats } = request.body;
 
-  const result = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: "You are an AI",
-      },
-      ...chats,
-    ],
-  });
+  try {
+    // Use the OpenAI API to generate chat completions
+    const result = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'system', content: 'You are an AI' },
+        ...chats,
+      ],
+    });
 
-  response.json({
-    output: result.data.choices[0].message,
-  });
+    response.json({
+      output: result.data.choices[0].message.content,
+    });
+  } catch (error) {
+    response.status(500).json({ error: 'An error occurred while processing the request.' });
+  }
 });
 
+// Start the server
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
